79cc15793a4f7b952edb102f7cafa77236d8e70b
Jeevan <javaindustryexperience@gmail.com>
2024-08-08 08:17:39 +0530

    ArrayList code added here

---
+++ TestMayBatchDemo/src/com/velocity/SampleTest.java
@@ -0,0 +1,17 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		list.add("mumbai");
+		list.add("pune");
+		list.add("bangalore");
+		list.add("delhi");
+		System.out.println(list);
+	}
+}
